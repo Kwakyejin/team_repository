b9b605cd50dd0b6f88fba13db6263aabce79de4c
subeom0 <ansubeom0@gmail.com>
2021-04-11 17:01:14 +0900

    test

---
+++ change_money.py
@@ -0,0 +1,5 @@
+def change(Kr_money):
+    Kr_money = float(Kr_money)
+    money_list = [Kr_money/float(1123.06), Kr_money/float(1336.53), Kr_money/float(10.22), Kr_money/float(171.38)]
+    return money_list
+    
