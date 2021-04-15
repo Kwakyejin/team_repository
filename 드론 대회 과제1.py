446ad3ab005b11d760b35c3e51f9e914e92231b9
Kang-Junhui <72507070+Kang-Junhui@users.noreply.github.com>
2021-04-15 18:45:50 +0900

    Update 드론 대회 과제1.py

--- 드론 대회 과제1.py
+++ 드론 대회 과제1.py
@@ -1,25 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[19]:
-
-
 a = input('한국 원화 단위의 돈을 입력하시오. ')
 b = a.replace('원','').replace('KRW','')
 print(b)
 
-
-# In[ ]:
-
-
 #change-환전
 #Count_money-변환 
-change(c)
-Count_money(c)
-
-
-# In[ ]:
-
-
-Count_money(c)
-
+change(b)
+Count_money(b)
