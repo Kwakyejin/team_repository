b41ff4234f14503741e78dda636f599d140c5141
subeom0 <76480504+subeom0@users.noreply.github.com>
2021-07-05 23:59:13 +0900

    Update drone.py

--- drone.py
+++ drone.py
@@ -6,78 +6,93 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 
-#sendControlPosition(self, positionX, positionY, positionZ, velocity, heading, rotationalVelocity):
+
+# sendControlPosition(self, positionX, positionY, positionZ, velocity, heading, rotationalVelocity):
+
+def initialize():
+    drone = Drone()
+    drone.open()
+    return drone
+
 
 def capture_img():
     camera = PiCamera()
     img = 'img.jpg'
-    camera.resolution = (240, 240)#160, 128
+    camera.resolution = (240, 240)  # 160, 128
     camera.framerate = 32
     camera.rotation = 180
     camera.capture(img)
     camera.close()
-    return img # log
+    return img  # 캡쳐 이미지 경로
 
-def find_centroid(): # centroid = 240x240 in (480x480) // need to recheck
-    drone = Drone()
+
+def find_centroid(drone):  # centroid = 240x240 in (480x480) // need to recheck
     lower_green = np.array([50, 128, 50])
     upper_green = np.array([80, 255, 255])
-    # k = 0
+
     img = cv2.imread(capture_img())
     img = cv2.GaussianBlur(img, (9, 9), 3)
-    # img = cv2.resize(img, dsize=(480,480))
+
+    # img = cv2.resize(img, dsize=(240,240))
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_green, upper_green)
-    _,contours,hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # img, cont, hier / https://opencv-python.readthedocs.io/en/latest/doc/15.imageContours/imageContours.html
+
+    _, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
     print(contours, hierarchy[0], len(hierarchy[0]))
-    img = cv2.drawContours(img, contours, 0, (255,255,0), 3)
-    if len(hierarchy) <= 1:
+
+    # img = cv2.drawContours(img, contours, -1, (255,255,0), 3)
+    if len(hierarchy[0]) <= 1 or None:
         print("go back")
+
         cv2.imshow('img', img)
+        cv2.imshow('mask', mask)
         cv2.waitKey(0)
-        time.sleep(5)
-        # drone.sendControlPosition(0, 0.1, 0, 0.1, 0, 0)
-        find_centroid()
+
+        drone.sendLanding()
+        print('w')
+        cv2.waitKey(0)
+        # drone.sendControlPosition(0.2, 0, 0, 1, 0, 0)
+        time.sleep(3)
+        find_centroid(drone)
     else:
-        cnt = contours[0]
+        cnt = contours[1]
+        img = cv2.drawContours(img, contours, 1, (255, 255, 0), 3)
         M = cv2.moments(cnt)
-        cx = int(M['m10'] / (M['m00'] + 0.000001))
-        cy = int(M['m01'] / (M['m00'] + 0.000001))
-        # for i in contours:
-        #     for j in range(len(i)):
-        #         if k == 0:
-        #             # pass
-        #             cv2.circle(img, (int(i[j][0][0]), int(i[j][0][1])), 4, (0, 0, 255), -1)
-        #         else:
-        #             cv2.circle(img, (int(i[j][0][0]), int(i[j][0][1])), 4, (255, 255, 255), -1)
-        #     k = 1
-        # cv2.circle(img, (cx, cy), 4, (255, 255, 0), -1)
-        # cv2.imshow('asdasd', img)
-        # cv2.imshow('mask', mask)
-        # cv2.waitKey(0)
+        cx = int(M['m10'] / (M['m00'] + 0.000000000000001))
+        cy = int(M['m01'] / (M['m00'] + 0.000000000000001))
         cv2.circle(img, (cx, cy), 4, (255, 255, 0), -1)
+        print(cx, cy)
         cv2.imshow('img', img)
+        cv2.imshow('mask', mask)
         cv2.waitKey(0)
         return cx, cy
 
-def check_distance():
-    drone = Drone()
-    cx, cy = find_centroid()
-    print('first ok')
-    #drone.sendControlPosition(0, 0.1, 0, 0.1, 0, 0)
-    cx2, cy2 = find_centroid()
-    print('second ok')
-    change_f = abs(cx - cx2)# cx_2
-    m_per_f = 0.1/change_f
-    x = 120 - cx2 # x > 0 go right x < 0 go left
-    y = 120 - cy2 # y > 0 go up y < 0 go down
-    print(cx,cy,cx2,cy2)
-    return x*m_per_f, y*m_per_f
 
-def move_to_center(x, y):
+def check_distance(drone):
+    cx, cy = find_centroid(drone)
+    mx = -0.15 if cx >= 120 else 0.15
+    my = -0.15 if cy >= 140 else 0.15
+
+    print('first ok')
+    sleep(10)
+    drone.sendControlPosition(0, mx, my, 1, 0, 0)
+    print('move')
+    time.sleep(5)
+    cx2, cy2 = find_centroid(drone)
+    print('second ok')
+    # change_f = abs(cx - cx2)# cx_2
+    # m_per_f = 0.2/change_f
+    # x = 120 - cx2 # x > 0 go right x < 0 go left
+    # y = 120 - cy2 # y > 0 go up y < 0 go down
+    print(cx, cy, cx2, cy2)  # ,m_per_f)
+    return 0.3 * (cx2 - 120) / (cx - cx2), 0.3 * (cy2 - 140) / (cy - cy2)  # x*m_per_f, y*m_per_f
+
+
+def move_to_center(drone, x, y):
     print('move to center')
-    drone = Drone()
-    #drone.sendControlPosition(0, -x, y, 1, 0, 0) # +y = left -y = right
+    drone.sendControlPosition(0, x, y, 1, 0, 0)  # +y = left -y = right
+
 
 def pass_obstacle():
     pass
