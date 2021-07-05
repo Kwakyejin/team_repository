0f72b28f69d5a92f3c38f6998727728483be904e
subeom0 <76480504+subeom0@users.noreply.github.com>
2021-07-05 10:35:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from drone import *
 import cv2
 import numpy as np
+from e_drone.drone import *
+from e_drone.protocol import *
+import time
 
-img = capture_img()
-x,y = check_distance(img)
-move_to_center(x,y)
+
+drone = Drone()
+drone.open()
+# drone.sendTakeOff()
+print('check')
+time.sleep(1)
+x, y = check_distance()
+move_to_center(x, y)
+print('finish')
+time.sleep(5)
+# drone.sendLanding()
 # pass_obstacle()
