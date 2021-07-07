053c45f7e6572c53a4eb677dbe2c29aac5866496
subeom0 <76480504+subeom0@users.noreply.github.com>
2021-07-07 13:56:22 +0900

    Update drone.py

--- drone.py
+++ drone.py
@@ -44,12 +44,11 @@ def find_centroid(drone):  # centroid = 240x240 in (480x480) // need to recheck
     # img = cv2.drawContours(img, contours, -1, (255,255,0), 3)
     if len(hierarchy[0]) <= 1 or None:
         print("go back")
-
         cv2.imshow('img', img)
         cv2.imshow('mask', mask)
         cv2.waitKey(0)
 
-        drone.sendControlPosition(-1, 0, 0, 1, 0, 0)
+        drone.sendControlPosition(-0.5, 0, 0, 1, 0, 0)
         time.sleep(3)
         find_centroid(drone)
     else:
@@ -69,12 +68,12 @@ def find_centroid(drone):  # centroid = 240x240 in (480x480) // need to recheck
 def check_distance(drone):
     cx, cy = find_centroid(drone)
     mx = -0.15 if cx >= 120 else 0.15
-    my = -0.15 if cy >= 140 else 0.15
+    my = -0.15 if cy >= 150 else 0.15
 
     print('first ok')
-    sleep(10)
-    drone.sendControlPosition(0, mx, my, 1, 0, 0)
+    time.sleep(5)
     print('move')
+    drone.sendControlPosition(0, mx, my, 1, 0, 0)
     time.sleep(5)
     cx2, cy2 = find_centroid(drone)
     print('second ok')
@@ -83,14 +82,14 @@ def check_distance(drone):
     # x = 120 - cx2 # x > 0 go right x < 0 go left
     # y = 120 - cy2 # y > 0 go up y < 0 go down
     print(cx, cy, cx2, cy2)  # ,m_per_f)
-    return 0.3 * (cx2 - 120) / (cx - cx2), 0.3 * (cy2 - 140) / (cy - cy2)  # x*m_per_f, y*m_per_f
+    return 0.15 * (cx2 - 120) / (cx - cx2), 0.15 * (cy2 - 150) / (cy - cy2)  # x*m_per_f, y*m_per_f
 
 
 def move_to_center(drone, x, y):
     print('move to center')
     drone.sendControlPosition(0, x, y, 1, 0, 0)  # +y = left -y = right
     time.sleep(5)
-    pass_obstacle(drone)
+    #pass_obstacle(drone)
 
 
 def find_redpoint():
@@ -120,14 +119,14 @@ def find_purplepoint():
 
 def pass_obstacle(drone):
     if find_purplepoint() > 200:
-        print('detect perple point')
+        print('detect purple point')
         drone.sendLanding()
         drone.close()
         return 0
 
     if find_redpoint() < 200:
         drone.sendControlPosition(1, 0, 0, 1, 0, 0)
-        print('not find red(perple) point')
+        print('not find red(purple) point')
         time.sleep(3)
         pass_obstacle(drone)
 
@@ -136,9 +135,4 @@ def pass_obstacle(drone):
         print('find red point')
         time.sleep(5)
         return 0
-        
-    else:
-        drone.sendControlPosition(0, 0, 0, 0, 90, 18)
-        print('find red point')
-        time.sleep(5)
-        return 0
+
